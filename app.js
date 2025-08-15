e393dd690d345f1d5773ab735692a8f32fbea6cb
BenjaminLuza <benjaminluza@gmail.com>
2025-08-15 19:42:02 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosSorteo=[];
+let nombreAmigo=0;
+//1- FUNCIÓN PARA AGREGAR AMIGOS
+function agregarAmigo(){
+nombreAmigo = document.getElementById("amigo").value;
+    
+if (nombreAmigo=="") {
+    alert("Por favor, inserte un nombre");
+} else {
+    amigosSorteo.push(nombreAmigo);
+    console.log(amigosSorteo);
+    document.getElementById("amigo").value="";
+    listarAmigos();
+}
+
+}
+//FUNCIÓN PARA AGREGAR LISTA DE AMIGOS
+function listarAmigos() {
+  let lista=document.getElementById("listaAmigos");
+  lista.innerHTML="";
+for (let i = 0; i < amigosSorteo.length; i++) {
+    let li = document.createElement("li");
+    li.textContent=amigosSorteo[i];
+    lista.appendChild(li);
+    
+}
+    }
+
+
+
